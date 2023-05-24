29e5273aa084f705016c691c26495d5e6ed5e8df
mohitrawal0424 <112812307+mohitrawal0424@users.noreply.github.com>
2023-05-25 02:24:05 +0530

    index, customer login page

--- index1.php
+++ index1.php
@@ -1,13 +1,13 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tamanna Khadyan Bhandar</title> -->
-
+    <title>Admin DashBoard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <!--google font start-->
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Caladea:ital@1&family=Varela+Round&display=swap" rel="stylesheet">
 <style>
@@ -15,48 +15,93 @@
     font-family: 'Caladea', serif;
   font-family: 'Varela Round', sans-serif;
   }
-  </style> -->
+  </style>
   <!--google font end-->
-<!-- </head>
-<body>
-    <h1>Homepage</h1>
     
-</body>
-</html> -->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Tamanna Khadyan</a>
+ <!--Header start-->
+ <nav class="bg-gray-900 w-full z-20 border-b border-gray-200 border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">TamannaKhadyan</span>
+    </a>
+    <div class="flex md:order-2">
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"><a href="customers/login.php">Login</a></button>
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+          <!-- <span class="sr-only">Open main menu</span> -->
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact </a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#" style="background-color:grey;"><span class="glyphicon glyphicon-user"></span> Log In</a></li>
-      <li><a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
-    </ul>
-  </div>
-</nav>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium text-sm border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-900">
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0" aria-current="page">Contact Us</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About Us</a>
+        </li>
+        
+      </ul>
+    </div>
+    </div>
+  </nav>
+<!--header end-->
+    <!--hero section -->
+  <!-- Hero -->
+<div class="bg-slate-900">
+  <div class="bg-gradient-to-b from-violet-600/[.15] via-transparent pt-20 pb-20">
+    <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
+      <!-- Announcement Banner -->
+      <!-- <div class="flex justify-center">
+        <a class="group inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 pl-4 rounded-full shadow-md" href="../figma.html">
+          <p class="mr-2 inline-block text-white text-sm">
+            Preline UI Figma is live.
+          </p>
+          <span class="group-hover:bg-white/[.1] py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-full bg-white/[.075] font-semibold text-white text-sm">
+            <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </span>
+        </a>
+      </div> -->
+      <!-- End Announcement Banner -->
 
-<!-- <div class="container">
-  <h3>Right Aligned Navbar</h3>
-  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
-</div> -->
+      <!-- Title -->
+      <div class="max-w-3xl text-center mx-auto">
+        <h1 class="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Now it's easier to Manage Track your Loans
+        </h1>
+      </div>
+      <!-- End Title -->
+
+      <div class="max-w-3xl text-center mx-auto">
+        <p class="text-lg text-gray-400">Add any Text You want to add (hey developers)</p>
+      </div>
+
+      <!-- Buttons -->
+      <div class="text-center">
+        <a href="customers/login.php" class="inline-flex justify-center text-xl items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 focus:ring-offset-gray-800" href="#">
+          Customers Login
+          <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </a>
+      </div>
+      <!-- End Buttons -->
+    </div>
+  </div>
+</div>
+<!-- End Hero -->
+
+
+<!-- Footer start-->
+<?php 
+include "include/footer.php"; 
+?>
+<!-- Footer End-->
+
 
 </body>
 </html>
