8f87040ebe2b5f1528893179539c934e62f21a35
mohitrawal0424 <112812307+mohitrawal0424@users.noreply.github.com>
2023-05-27 15:03:58 +0530

    table created for fetching in customer.php

--- admin/customers.php
+++ admin/customers.php
@@ -17,10 +17,43 @@ include "../include/navbar.php";
 </button>
 
 <h1>fetch customer data here</h1>
-<h2>fetch here</h2>
 
-<h1>fetch here</h1>
-<h1>fetch here</h1>
-<h1>fetch here</h1>
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-400">
+        <thead class="text-xs uppercase bg-gray-700 text-gray-200">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Color
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Category
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-b bg-gray-800 border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+                <td class="px-6 py-4">
+                    Laptop
+                </td>
+                <td class="px-6 py-4">
+                    $2999
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 </body>
 </html>
