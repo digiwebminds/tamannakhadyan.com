05cdade96175e0d138e4e7427d53e8d4e7c46085
mohitrawal0424 <112812307+mohitrawal0424@users.noreply.github.com>
2023-05-28 23:45:04 +0530

    cc loan layout and database connect done

--- admin/add_user.php
+++ admin/add_user.php
@@ -85,7 +85,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script> 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="../include/js/add_user.js"></script>
 
