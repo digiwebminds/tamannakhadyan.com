bf300dce7db855ad2c5e4089f304b8394f904952
vickysoun <vickybisht524@gmail.com>
2023-05-31 01:39:18 +0530

    customers table with pagenation

--- admin/customers.php
+++ admin/customers.php
@@ -1,59 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
     <title>Add User</title>
     <script src="../include/js/add_user.js"></script>
 </head>
+
 <body>
     <?php
-include "../include/navbar.php";
+    include "../include/navbar.php";
     ?>
-    <button href="add_user.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="add_user.php">  Add User </a>
- 
-</button>
+    <button href="add_user.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="add_user.php">Add User</a></button>
 
-<h1>fetch customer data here</h1>
+    <h1>List of Customers</h1>
 
-<div class="relative overflow-x-auto">
-    <table class="w-full text-sm text-left text-gray-400">
-        <thead class="text-xs uppercase bg-gray-700 text-gray-200">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Product name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="border-b bg-gray-800 border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+    <div class="relative" id="pagination-result">
+        <div id="overlay">
+            <div><img src="../include/loading.gif" width="64px" height="64px" /></div>
+        </div>
+
+    </div>
+
+    <?php
+    include("../include/connect.php");
+    $query = "SELECT * FROM customers";
+    $result  = mysqli_query($conn, $query);
+    $rowcount = mysqli_num_rows($result);
+    echo '<input type="hidden" name="rowcount" id="rowcount" value=' . $rowcount . '>';
+    ?>
+    <script>
+        function getresult(url) {
+            $.ajax({
+                url: url,
+                type: "GET",
+                data: {
+                    rowcount: $("#rowcount").val()
+                },
+                beforeSend: function() {
+                    $("#overlay").show();
+                },
+                success: function(data) {
+                    $("#pagination-result").html(data);
+                    setInterval(function() {
+                        $("#overlay").hide();
+                    }, 500);
+                }
+            });
+        }
+        getresult("custajax.php");
+    </script>
+
 
 </body>
+
 </html>
