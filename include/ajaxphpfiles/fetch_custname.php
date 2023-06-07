6bbd91cc618fe5fe85282e40363987d41acede57
mohitrawal0424 <112812307+mohitrawal0424@users.noreply.github.com>
2023-06-08 01:00:39 +0530

    somthing

--- include/ajaxphpfiles/fetch_custname.php
+++ include/ajaxphpfiles/fetch_custname.php
@@ -208,28 +208,37 @@ echo '<div id="myModalrepayprinciple" class="modal hidden fixed inset-0 flex ite
   
 <form action="" method="POST">
               <div class="grid gap-4 sm:grid-cols-2 sm:gap-6 p-4">
-                  
                   <div>
-                      <label for="dorepayprinciple" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">D.O.Repayment Principle</label>
-                      <input type="date" name="dorepayprinciple" id="dorepayprinciple" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                      <label for="dorepay-principal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">D.O.Repayment</label>
+                      <input type="date" name="dorepay-principal" id="dorepay-principal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                   </div>
                   <div>
                       <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Loan ID</label>
-                      <input type="number" name="loan_id" id="loan_id" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
+                      <input type="number" name="loan_id" id="loan_id" value="' . $row['id'] . '" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
                   </div>
                   <div>
                       <label for="loantype" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Loan Type</label>
-                      <input type="text" name="loantype" id="loantype" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
+                      <input type="text" name="loantype" id="loantype" value="';
+
+      if ($loan_type == 1) {
+        echo "CC Loan";
+      } elseif ($loan_type == 2) {
+        echo "Daily Loan";
+      } elseif ($loan_type == 3) {
+        echo "Weekly Loan";
+      } else {
+        echo "Monthly Loan";
+      }
+
+      echo '" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
                   </div>
                   <div>
-                      <label for="principle-amount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Installment Amount</label>
-                      <input type="number" name="principle-amount" id="principle-amount" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                      <label for="principle-amount-repay" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Principle Amount</label>
+                      <input type="number" name="principle-amount-repay" id="principle-amount-repay" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                   </div>
-
-                  <div id="repaymentprinciplealert"></div>
-
-                  <button type="submit" id="repayprinciplesubmitbtnn" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                      Pay Installment
+                  <div id="principlerepayalert"></div>
+                  <button type="submit" id="repay-principle-submitbtnn" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                      Pay Principle
                   </button>
               </div>
           </form>
@@ -311,7 +320,27 @@ if (isset($_POST['dorepay']) && isset($_POST['loan_id']) && isset($_POST['instal
     <span class="font-medium">Alert !</span> There is some problem Please try Again !
   </div>';
   }
- 
+}
+
+// code to enter principle repayment details in database 
+
+if (isset($_POST['dorepay']) && isset($_POST['loan_id']) && isset($_POST['principleamt'])) {
+  require_once "../connect.php";
+  $dorepay = $_POST['dorepay'];
+  $loan_id = $_POST['loan_id'];
+  $principleamt = $_POST['principleamt'];
+
+  $sql = "INSERT INTO `principle-repayment` (`loan_id`, `dorepayment`, `repay-amount`) VALUES ('$loan_id', '$dorepay', '$principleamt')";
+  $result = mysqli_query($conn,$sql);
+  if($result){
+    echo '<div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+    <span class="font-medium">Success !</span> Repayment Done Successfully
+  </div>';
+  }else{
+    echo '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+    <span class="font-medium">Alert !</span> There is some problem Please try Again !
+  </div>';
+  }
 }
 
 
